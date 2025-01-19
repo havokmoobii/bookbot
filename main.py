b65488c7810890764fde740f7e7b92050cb05bf4
HavokMoobii <jasonmcounter@gmail.com>
2025-01-18 19:43:26 -0600

    Added function to count all characters in a document and print their frequency.

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():    
+def main():
+    file_contents = 0
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+
+    words = file_contents.split()
+    print(len(words))
+    print(count_characters(file_contents))
+
+def count_characters(document):
+    document = document.lower()
+
+    out_dic = {}
+
+    for character in document:
+        if out_dic.get(character) is None:
+            out_dic[character] = 0
+        out_dic[character] += 1
+
+    return(out_dic)
 
 main()
