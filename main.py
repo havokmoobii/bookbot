e333fa51715d92abff882cf7134fe6438018a6f8
HavokMoobii <jasonmcounter@gmail.com>
2025-01-19 00:56:37 -0600

    Added functions that generate a book report.

--- main.py
+++ main.py
@@ -2,12 +2,39 @@ def main():
     file_path = "books/frankenstein.txt"
     file_contents = get_book_text(file_path)
 
-    print(get_word_count(file_contents))
-    print(get_char_count(file_contents))
+    print_report(file_path, file_contents)
+
+def print_report(file_path, document):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{get_word_count(document)} words found in document.")
+    print()
+
+    char_dict = sort_dict(document)
+
+    for char_entry in char_dict:
+        print(f"The '{char_entry["name"]}' character was found {char_entry["num"]} times")
+
+    print("--- End report ---")
 
 def get_word_count(document):
     words = document.split()
     return len(words)
+
+def sort_dict(document):
+    char_count = get_char_count(document)
+
+    char_list = []
+
+    for char_entry in char_count:
+        if char_entry.isalpha():
+            char_list.append({"name": char_entry, "num": char_count[char_entry]})
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
+
+def sort_on(dict):
+    return dict["num"]
 
 def get_char_count(document):
     document = document.lower()
